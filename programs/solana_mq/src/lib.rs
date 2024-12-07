61f6d0f0ca14ea4e6e6d6f17d17f2194cc01c62b
MishkaRogachev <mishkarogachev@gmail.com>
2024-12-07 14:40:11 +0100

    feat: add simple `create_topic` method

--- programs/solana_mq/src/lib.rs
+++ programs/solana_mq/src/lib.rs
@@ -2,15 +2,41 @@ use anchor_lang::prelude::*;
 
 declare_id!("9tgFGPhuMsuXUUAWFNfuFEpBuANtjvwDdu9maFVFbLfo");
 
+const ANCHOR_DESCRIMINATOR_SIZE: usize = 8;
+
 #[program]
 pub mod solana_mq {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
-        msg!("Greetings from: {:?}", ctx.program_id);
+    pub fn create_topic(ctx: Context<CreateTopic>, topic_name: String) -> Result<()> {
+        let user_pubkey = ctx.accounts.user.key();
+        msg!("Creating topic {} for user {}", topic_name, user_pubkey);
+
+        ctx.accounts.topics.topics.push(topic_name);
         Ok(())
     }
 }
 
+#[account]
+#[derive(InitSpace)]
+pub struct Topics {
+    #[max_len(64, 64)]
+    pub topics: Vec<String>,
+}
+
 #[derive(Accounts)]
-pub struct Initialize {}
+pub struct CreateTopic<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        init,
+        payer = user,
+        space = ANCHOR_DESCRIMINATOR_SIZE + Topics::INIT_SPACE,
+        seeds = [b"topics", user.key().as_ref()],
+        bump
+    )]
+    pub topics: Account<'info, Topics>,
+
+    pub system_program: Program<'info, System>,
+}
--- tests/solana_mq.ts
+++ tests/solana_mq.ts
@@ -1,16 +1,37 @@
 import * as anchor from "@coral-xyz/anchor";
 import { Program } from "@coral-xyz/anchor";
 import { SolanaMq } from "../target/types/solana_mq";
+import { assert } from "chai";
 
 describe("solana_mq", () => {
-  // Configure the client to use the local cluster.
-  anchor.setProvider(anchor.AnchorProvider.env());
+  const provider = anchor.AnchorProvider.env();
+  anchor.setProvider(provider);
 
   const program = anchor.workspace.SolanaMq as Program<SolanaMq>;
 
-  it("Is initialized!", async () => {
-    // Add your test here.
-    const tx = await program.methods.initialize().rpc();
-    console.log("Your transaction signature", tx);
+  it("Topic CRUD operations", async () => {
+    const user = provider.wallet.publicKey;
+    const topicName = "MyTestTopic";
+
+    // Derive the PDA for the Topics account
+    const [topicsPda] = await anchor.web3.PublicKey.findProgramAddress(
+      [Buffer.from("topics"), user.toBuffer()],
+      program.programId
+    );
+
+    // Call the createTopic method
+    await program.methods
+      .createTopic(topicName)
+      .accounts({
+        user,
+      })
+      .rpc();
+    console.log("Topic created successfully.");
+
+    const response = await program.account.topics.fetch(topicsPda);
+    assert.ok(response.topics.length > 0, "No topics found.");
+    assert.ok(response.topics.at(0) === topicName, "Topic name mismatch.");
+
+    console.log("Topics: ", response.topics);
   });
 });

c9edf447360b8bf7de253fa5c8b6742b6e99a217
MishkaRogachev <mishkarogachev@gmail.com>
2024-12-07 19:36:14 +0100

    feat: separate `initialise` from `create_topic`, add `deinitialise`

--- programs/solana_mq/src/lib.rs
+++ programs/solana_mq/src/lib.rs
@@ -3,14 +3,38 @@ use anchor_lang::prelude::*;
 declare_id!("9tgFGPhuMsuXUUAWFNfuFEpBuANtjvwDdu9maFVFbLfo");
 
 const ANCHOR_DESCRIMINATOR_SIZE: usize = 8;
+const MAX_TOPICS: usize = 128;
+const MAX_TOPIC_LENGTH: usize = 64;
 
 #[program]
 pub mod solana_mq {
     use super::*;
 
+    pub fn initialise(ctx: Context<Initialise>) -> Result<()> {
+        msg!("Initialising Topics account for user {}", ctx.accounts.user.key());
+        Ok(())
+    }
+
+    pub fn deinitialise(ctx: Context<Deinitialise>) -> Result<()> {
+        msg!("Closing Topics account for user {}", ctx.accounts.user.key());
+        Ok(())
+    }
+
     pub fn create_topic(ctx: Context<CreateTopic>, topic_name: String) -> Result<()> {
         let user_pubkey = ctx.accounts.user.key();
         msg!("Creating topic {} for user {}", topic_name, user_pubkey);
+
+        if topic_name.len() > MAX_TOPIC_LENGTH {
+            return Err(ErrorCode::TopicNameTooLong.into());
+        }
+
+        if ctx.accounts.topics.topics.len() >= MAX_TOPICS {
+            return Err(ErrorCode::TopicLimitReached.into());
+        }
+
+        if ctx.accounts.topics.topics.contains(&topic_name) {
+            return Err(ErrorCode::TopicAlreadyExists.into());
+        }
 
         ctx.accounts.topics.topics.push(topic_name);
         Ok(())
@@ -20,16 +44,12 @@ pub mod solana_mq {
         let user_pubkey = ctx.accounts.user.key();
         msg!("Removing topic {} for user {}", topic_name, user_pubkey);
 
-        let mut topics = ctx.accounts.topics.topics.clone();
-        match topics.iter().position(|x| x == &topic_name) {
-            Some(index) => {
-                topics.remove(index);
-                ctx.accounts.topics.topics = topics;
-                Ok(())
-            }
-            None => {
-                return Err(ErrorCode::TopicNotFound.into());
-            }
+        let topics = &mut ctx.accounts.topics.topics;
+        if let Some(index) = topics.iter().position(|p| p == &topic_name) {
+            topics.remove(index);
+            Ok(())
+        } else {
+            Err(ErrorCode::TopicNotFound.into())
         }
     }
 }
@@ -37,12 +57,12 @@ pub mod solana_mq {
 #[account]
 #[derive(InitSpace)]
 pub struct Topics {
-    #[max_len(64, 64)]
+    #[max_len(MAX_TOPICS, MAX_TOPIC_LENGTH)]
     pub topics: Vec<String>,
 }
 
 #[derive(Accounts)]
-pub struct CreateTopic<'info> {
+pub struct Initialise<'info> {
     #[account(mut)]
     pub user: Signer<'info>,
 
@@ -58,6 +78,31 @@ pub struct CreateTopic<'info> {
     pub system_program: Program<'info, System>,
 }
 
+#[derive(Accounts)]
+pub struct Deinitialise<'info> {
+    #[account(mut)]
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        close = user, // Transfer remaining lamports to the user
+        seeds = [b"topics", user.key().as_ref()],
+        bump
+    )]
+    pub topics: Account<'info, Topics>,
+}
+
+#[derive(Accounts)]
+pub struct CreateTopic<'info> {
+    pub user: Signer<'info>,
+
+    #[account(
+        mut,
+        seeds = [b"topics", user.key().as_ref()],
+        bump
+    )]
+    pub topics: Account<'info, Topics>,
+}
 #[derive(Accounts)]
 pub struct RemoveTopic<'info> {
     #[account(mut)]
@@ -76,4 +121,7 @@ pub struct RemoveTopic<'info> {
 #[error_code]
 pub enum ErrorCode {
     TopicNotFound,
+    TopicNameTooLong,
+    TopicAlreadyExists,
+    TopicLimitReached,
 }
--- tests/solana_mq.ts
+++ tests/solana_mq.ts
@@ -3,6 +3,19 @@ import { Program } from "@coral-xyz/anchor";
 import { SolanaMq } from "../target/types/solana_mq";
 import { assert } from "chai";
 
+async function airdrop(provider, user, lamports) {
+  const airdropSignature = await provider.connection.requestAirdrop(user, lamports);
+  const latestBlockHash = await provider.connection.getLatestBlockhash();
+
+  await provider.connection.confirmTransaction({
+    blockhash: latestBlockHash.blockhash,
+    lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
+    signature: airdropSignature,
+  });
+
+  console.log(`Airdropped ${lamports / anchor.web3.LAMPORTS_PER_SOL} SOL to: ${user.toBase58()}`);
+}
+
 describe("solana_mq", () => {
   const provider = anchor.AnchorProvider.env();
   anchor.setProvider(provider);
@@ -10,69 +23,103 @@ describe("solana_mq", () => {
   const program = anchor.workspace.SolanaMq as Program<SolanaMq>;
 
   it("Topic CRUD operations", async () => {
-    // Generate a random keypair for the user
     const userKeypair = anchor.web3.Keypair.generate();
     const user = userKeypair.publicKey;
-    const topicName = "MyTestTopic";
+    const firstTopic = "/test_topic_1";
+    const secondTopic = "/test_topic_2";
 
-    // Airdrop SOL to the new user
-    const airdropSignature = await provider.connection.requestAirdrop(
-      user,
-      anchor.web3.LAMPORTS_PER_SOL // 1 SOL
-    );
+    // Airdrop!
+    await airdrop(provider, user, 1 * anchor.web3.LAMPORTS_PER_SOL);
 
-    const latestBlockHash = await provider.connection.getLatestBlockhash();
-
-    await provider.connection.confirmTransaction({
-      blockhash: latestBlockHash.blockhash,
-      lastValidBlockHeight: latestBlockHash.lastValidBlockHeight,
-      signature: airdropSignature,
-    });
-
-    console.log(`Airdropped 1 SOL to: ${user.toBase58()}`);
-
-    // Get initial balance
-    const initialBalance = await provider.connection.getBalance(user);
+    // Check initial balance
+    let balance = await provider.connection.getBalance(user);
+    console.log(`Initial balance: ${balance / anchor.web3.LAMPORTS_PER_SOL} SOL`);
 
     // Derive the PDA for the Topics account
-    const [topicsPda] = await anchor.web3.PublicKey.findProgramAddress(
+    const [topicsPda] = anchor.web3.PublicKey.findProgramAddressSync(
       [Buffer.from("topics"), user.toBuffer()],
       program.programId
     );
 
-    // Call the createTopic method
+    // Initialize Topics account
     await program.methods
-      .createTopic(topicName)
+      .initialise()
       .accounts({
         user,
       })
-      .signers([userKeypair]) // Include the userKeypair in the signers
+      .signers([userKeypair])
+      .rpc();
+
+    console.log("Topics account initialized.");
+    balance = await provider.connection.getBalance(user);
+    console.log(`Balance after initialization: ${balance / anchor.web3.LAMPORTS_PER_SOL} SOL`);
+
+    // Create first topic
+    await program.methods
+      .createTopic(firstTopic)
+      .accounts({
+        user,
+      })
+      .signers([userKeypair])
       .rpc();
 
     let response = await program.account.topics.fetch(topicsPda);
-    assert.ok(response.topics.length > 0, "No topics found.");
-    assert.ok(response.topics.at(0) === topicName, "Topic name mismatch.");
-    console.log("Topic created successfully.");
+    assert.strictEqual(response.topics.length, 1, "Should have 1 topic.");
+    assert.strictEqual(response.topics[0], firstTopic, "First topic mismatch.");
+    console.log("First topic created successfully.");
 
-    let balanceAfterCreate = await provider.connection.getBalance(user);
-    let createCost = (initialBalance - balanceAfterCreate) / anchor.web3.LAMPORTS_PER_SOL;
-    console.log(`Cost of createTopic: ${createCost} SOL`);
-
-    // Call the removeTopic
+    // Create second topic
     await program.methods
-      .removeTopic(topicName)
+      .createTopic(secondTopic)
       .accounts({
         user,
       })
-      .signers([userKeypair]) // Sign with the userKeypair
+      .signers([userKeypair])
       .rpc();
 
     response = await program.account.topics.fetch(topicsPda);
-    assert.ok(response.topics.length === 0, "Topic not removed.");
-    console.log("Topic removed successfully.");
+    assert.strictEqual(response.topics.length, 2, "Should have 2 topics.");
+    assert.strictEqual(response.topics[1], secondTopic, "Second topic mismatch.");
+    console.log("Second topic created successfully.");
 
-    let balanceAfterRemove = await provider.connection.getBalance(user);
-    let removeCost = (balanceAfterCreate - balanceAfterRemove) / anchor.web3.LAMPORTS_PER_SOL;
-    console.log(`Cost of removeTopic: ${removeCost} SOL`);
+    // Remove first topic
+    await program.methods
+      .removeTopic(firstTopic)
+      .accounts({
+        user,
+      })
+      .signers([userKeypair])
+      .rpc();
+
+    response = await program.account.topics.fetch(topicsPda);
+    assert.strictEqual(response.topics.length, 1, "Should have 1 topic.");
+    assert.strictEqual(response.topics[0], secondTopic, "First topic mismatch.");
+    console.log("First topic removed successfully.");
+
+    // Remove second topic
+    await program.methods
+      .removeTopic(secondTopic)
+      .accounts({
+        user,
+      })
+      .signers([userKeypair])
+      .rpc();
+
+    response = await program.account.topics.fetch(topicsPda);
+    assert.strictEqual(response.topics.length, 0, "Should have 0 topics.");
+    console.log("Second topic removed successfully.");
+
+    // Cleanup
+    await program.methods
+      .deinitialise()
+      .accounts({
+        user,
+      })
+      .signers([userKeypair])
+      .rpc();
+
+    console.log("Topics account deinitialised.");
+    balance = await provider.connection.getBalance(user);
+    console.log(`Final balance after deinitialisation: ${balance / anchor.web3.LAMPORTS_PER_SOL} SOL`);
   });
 });
